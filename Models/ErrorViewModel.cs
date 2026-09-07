c8cc239ed22f73d7bfcfb466a6b7e4aff99db9c1
ANIKA BR <ANIKA BR@DESKTOP-C4V5P6M>
2026-09-07 12:05:12 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentServiceRequestSystem1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
